7e8425fc436a1d02faf239d9a2696cc1f0b8f65a
KimMinSeok <kmsaa@naver.com>
2024-06-13 22:14:35 +0900

    feat: add ColorUtil

---
+++ lib/src/config/const/color_util.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorUtil {
+  /// 앱의 색상 정의
+  ColorUtil._();
+
+  static const Color primary = Color(0xFFFF6683);
+  static const Color green = Color(0xFF1FAF96);
+  static const Color orange = Color(0xFFFFA722);
+  static const Color border = Color(0xFFECECEE);
+  static const Color background = Color(0xFFF4F4F5);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // text color
+  static const Color mainText = Color(0xFF202022);
+  static const Color greyText = Color(0xFFA1A2AA);
+}
