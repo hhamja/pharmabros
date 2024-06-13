a0846aa902499fd7c3963598b4f112f30e322aa1
KimMinSeok <kmsaa@naver.com>
2024-06-13 22:43:34 +0900

    feat: Add MainTab routes

--- lib/src/config/routes/routes.dart
+++ lib/src/config/routes/routes.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pharmabros/src/common/page/main_tab.dart';
 
 final routes = {
   '/splash': (BuildContext context) => Container(),
-  '/tab': (BuildContext context) => Container(),
+  '/main_tab': (BuildContext context) => const MainTab(),
   '/product/detail': (BuildContext context) => Container(),
 };
