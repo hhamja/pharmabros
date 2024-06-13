9e5546ebbed68c3b24a504ccbab6e8ca3ea042d6
KimMinSeok <kmsaa@naver.com>
2024-06-13 22:45:41 +0900

    feat: set backgroud color to white

--- lib/src/common/page/splash_page.dart
+++ lib/src/common/page/splash_page.dart
@@ -25,6 +25,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
