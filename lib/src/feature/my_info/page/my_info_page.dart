7d8a843f36a3a1ec5aa94b92912be9b5663779fb
KimMinSeok <kmsaa@naver.com>
2024-06-14 07:51:54 +0900

    feat: add MyPage Mock UI

---
+++ lib/src/feature/my_info/page/my_info_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyInfoPage extends StatelessWidget {
+  /// 내정보 페이지
+  const MyInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
