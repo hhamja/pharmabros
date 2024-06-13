2b9b69766a43332c438d372d2c9c53ddc1451145
KimMinSeok <kmsaa@naver.com>
2024-06-13 22:43:54 +0900

    feat: Add SplashPage routes

--- lib/src/config/routes/routes.dart
+++ lib/src/config/routes/routes.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pharmabros/src/common/page/main_tab.dart';
+import 'package:pharmabros/src/common/page/splash_page.dart';
 
 final routes = {
-  '/splash': (BuildContext context) => Container(),
+  '/splash': (BuildContext context) => const SplashPage(),
   '/main_tab': (BuildContext context) => const MainTab(),
   '/product/detail': (BuildContext context) => Container(),
 };
